5cf9055e6a9e197a681678cdb56b7becad529b08
captainSun <sun_shuo825@163.com>
2023-05-18 18:05:06 +0800

    添加打包模块

---
+++ Assets/LuaFramework/Scripts/Utility/Util.cs
@@ -0,0 +1,282 @@
+﻿#define DO_NOT_CHECK_ENVI
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System.Reflection;
+using LuaInterface;
+using System.Xml;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace LuaFramework
+{
+    public class Util
+    {
+        public static int Int(object o)
+        {
+            return Convert.ToInt32(o);
+        }
+
+        public static float Float(object o)
+        {
+            return (float)Math.Round(Convert.ToSingle(o), 2);
+        }
+
+        public static long Long(object o)
+        {
+            return Convert.ToInt64(o);
+        }
+
+        public static int Random(int min, int max)
+        {
+            return UnityEngine.Random.Range(min, max);
+        }
+
+        public static float Random(float min, float max)
+        {
+            return UnityEngine.Random.Range(min, max);
+        }
+
+        public static string Uid(string uid)
+        {
+            int position = uid.LastIndexOf('_');
+            return uid.Remove(0, position + 1);
+        }
+
+        public static long GetTime()
+        {
+            TimeSpan ts = new TimeSpan(DateTime.UtcNow.Ticks - new DateTime(1970, 1, 1, 0, 0, 0).Ticks);
+            return (long)ts.TotalMilliseconds;
+        }
+
+
+        /// <summary>
+        /// 查找子对象
+        /// </summary>
+        public static GameObject Child(GameObject go, string subnode)
+        {
+            return Child(go.transform, subnode);
+        }
+
+        /// <summary>
+        /// 查找子对象
+        /// </summary>
+        public static GameObject Child(Transform go, string subnode)
+        {
+            Transform tran = go.Find(subnode);
+            if (tran == null) return null;
+            return tran.gameObject;
+        }
+
+        /// <summary>
+        /// 取平级对象
+        /// </summary>
+        public static GameObject Peer(GameObject go, string subnode)
+        {
+            return Peer(go.transform, subnode);
+        }
+
+        /// <summary>
+        /// 取平级对象
+        /// </summary>
+        public static GameObject Peer(Transform go, string subnode)
+        {
+            Transform tran = go.parent.Find(subnode);
+            if (tran == null) return null;
+            return tran.gameObject;
+        }
+
+        /// <summary>
+        /// 计算字符串的MD5值
+        /// </summary>
+        public static string md5(string source)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(source);
+            byte[] md5Data = md5.ComputeHash(data, 0, data.Length);
+            md5.Clear();
+
+            string destString = "";
+            for (int i = 0; i < md5Data.Length; i++)
+            {
+                destString += System.Convert.ToString(md5Data[i], 16).PadLeft(2, '0');
+            }
+            destString = destString.PadLeft(32, '0');
+            return destString;
+        }
+
+        /// <summary>
+        /// 计算文件的MD5值
+        /// </summary>
+        public static string md5file(string file)
+        {
+            try
+            {
+                FileStream fs = new FileStream(file, FileMode.Open);
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(fs);
+                fs.Close();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("md5file() fail, error:" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 清除所有子节点
+        /// </summary>
+        public static void ClearChild(Transform go)
+        {
+            if (go == null) return;
+            for (int i = go.childCount - 1; i >= 0; i--)
+            {
+                GameObject.Destroy(go.GetChild(i).gameObject);
+            }
+        }
+        
+        /// <summary>
+        /// 取得数据存放目录
+        /// </summary>
+        public static string DataPath
+        {
+            get
+            {
+                string game = AppConst.AppName.ToLower();
+                if (Application.isMobilePlatform)
+                {
+                    return Application.persistentDataPath + "/" + game + "/";
+                }
+                if (Application.platform == RuntimePlatform.WindowsPlayer)
+                {
+                    return Application.streamingAssetsPath + "/";
+                }
+                if (AppConst.DebugMode && Application.isEditor)
+                {
+                    return Application.streamingAssetsPath + "/";
+                }
+                if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.WindowsEditor)
+                {
+                    //int i = Application.dataPath.LastIndexOf('/');
+                    //return Application.dataPath.Substring(0, i + 1) + game + "/";
+                    //return Application.dataPath + "/" + game + "/";
+                    return Application.streamingAssetsPath + "/";
+                }
+                return "c:/" + game + "/";
+            }
+        }
+
+        /// <summary>
+        /// 应用程序内容路径
+        /// </summary>
+        public static string AppContentPath()
+        {
+            string path = string.Empty;
+            switch (Application.platform)
+            {
+                case RuntimePlatform.Android:
+                    path = "jar:file://" + Application.dataPath + "!/assets/";
+                    break;
+                case RuntimePlatform.IPhonePlayer:
+                    path = Application.dataPath + "/Raw/";
+                    break;
+                default:
+                    path = Application.dataPath + "/StreamingAssets/";
+                    break;
+            }
+            return path;
+        }
+
+
+        public static void Log(string str)
+        {
+            GameLogger.Log(str);
+        }
+
+        public static void LogGreen(string str)
+        {
+            GameLogger.LogGreen(str);
+        }
+
+        public static void LogYellow(string str)
+        {
+            GameLogger.LogYellow(str);
+        }
+
+
+        public static void LogWarning(string str)
+        {
+            GameLogger.LogWarning(str);
+        }
+
+        public static void LogError(string str)
+        {
+            GameLogger.LogError(str);
+        }
+
+        public static Component AddComponent(GameObject go, string assembly, string classname)
+        {
+            Assembly asmb = Assembly.Load(assembly);
+            Type t = asmb.GetType(assembly + "." + classname);
+            return go.AddComponent(t);
+        }
+        
+        /// <summary>
+        /// 载入Prefab
+        /// </summary>
+        /// <param name="name"></param>
+        public static GameObject LoadPrefab(string name)
+        {
+            return Resources.Load(name, typeof(GameObject)) as GameObject;
+        }
+
+        /// <summary>
+        /// 防止初学者不按步骤来操作
+        /// </summary>
+        /// <returns></returns>
+        static int CheckRuntimeFile()
+        {
+            if (!Application.isEditor) return 0;
+            string streamDir = Application.dataPath + "/StreamingAssets/";
+            if (!Directory.Exists(streamDir))
+            {
+                return -1;
+            }
+            else
+            {
+                string[] files = Directory.GetFiles(streamDir);
+                if (files.Length == 0) return -1;
+
+                if (!File.Exists(streamDir + "files.txt"))
+                {
+                    return -1;
+                }
+            }
+            string sourceDir = AppConst.FrameworkRoot + "/ToLua/Source/Generate/";
+            if (!Directory.Exists(sourceDir))
+            {
+                return -2;
+            }
+            else
+            {
+                string[] files = Directory.GetFiles(sourceDir);
+                if (files.Length == 0) return -2;
+            }
+            return 0;
+        }
+        
+
+      
+    }
+}
