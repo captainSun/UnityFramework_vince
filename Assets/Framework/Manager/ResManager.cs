37fbf600d35c0401d05c340583fa2808ae041f90
captainSun <sun_shuo825@163.com>
2023-05-29 18:14:26 +0800

    修改ResManager

--- Assets/Framework/Manager/ResManager.cs
+++ Assets/Framework/Manager/ResManager.cs
@@ -16,18 +16,37 @@ namespace VinceFramework
         public void Init()
         {
             bundles = new Dictionary<string, AssetBundle>();
-            string manifestFilePath = "";
+            string manifestFilePath = Application.streamingAssetsPath + "/" + AppConst.AssetBundleDirName + "/" + AppConst.AssetBundleDirName;
             AssetBundle assetBundle = AssetBundle.LoadFromFile(manifestFilePath);
             manifest = assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
         }
-        
+
+        //加载指定AB包
+        public void LoadAssetBundle(string abName)
+        {
+            foreach (string path in manifest.GetAllDependencies(abName))
+            {
+                string fullPath = Application.streamingAssetsPath + "/" + AppConst.AssetBundleDirName + "/" + path;
+                AssetBundle temp;
+                if (bundles.TryGetValue(fullPath, out temp))
+                {
+                    break;
+                }
+                else
+                {
+                    AssetBundle ab = AssetBundle.LoadFromFile(fullPath);
+                    bundles.Add(fullPath, ab);
+                }
+            }
+        }
+
         //载入资源
         public T LoadAsset<T>(string assetPath) where T : UnityEngine.Object
         {
 #if UNITY_EDITOR
             return AssetDatabase.LoadAssetAtPath<T>(Application.dataPath + "/" + AppConst.ResDirPath + assetPath);
 #else
-            return Application.streamingAssetsPath
+            return null;
 #endif
         }
         
