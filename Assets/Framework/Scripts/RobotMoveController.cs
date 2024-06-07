37a2e5c07887b7dad50711bee5b7f4f48710fc16
captainSun <sun_shuo825@163.com>
2024-06-07 14:46:15 +0800

    修改机器人运动逻辑

--- Assets/Framework/Scripts/RobotMoveController.cs
+++ Assets/Framework/Scripts/RobotMoveController.cs
@@ -6,8 +6,10 @@ namespace Framework.Scripts
     {
         public float walkSpeed = 5f; // 行走速度
         public float runSpeed = 10f; // 奔跑速度
-        public float rotationSpeed = 5f; //转向速度
         Animator animator;
+        private bool isChange = false;
+
+        private CharacterController cc;
         // 定义不同的状态枚举
         enum MovementState
         {
@@ -21,6 +23,7 @@ namespace Framework.Scripts
         void Start()
         {
             animator = GetComponentInChildren<Animator>();
+            cc = GetComponent<CharacterController>();
         }
 
         void Update()
@@ -57,44 +60,40 @@ namespace Framework.Scripts
 
             if (currentState != MovementState.Idle)
             {
-                // // 根据输入计算移动
-                // float moveHorizontal = Input.GetAxis("Horizontal");
-                // float moveVertical = Input.GetAxis("Vertical");
-                // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
-                // float speed = currentState == MovementState.Walking ? walkSpeed : runSpeed;
-                // transform.Translate(movement * Time.deltaTime * speed);
-                // //
-                // // 计算移动方向并调整角色的朝向
-                // if (movement.magnitude > 0)
-                // {
-                //     Quaternion targetRotation = Quaternion.LookRotation(transform.TransformDirection(movement));
-                //     transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-                // }
-                Vector2 direction = Vector2.zero;
-                if (Input.GetKey(KeyCode.W))
-                {
-                    direction = new Vector2(0, 1);
-                }
-                if (Input.GetKey(KeyCode.S))
-                {
-                    direction = new Vector2(0, -1);
-                }
-                if (Input.GetKey(KeyCode.D))
-                {
-                    direction = new Vector2(1, 0);
-                }
-                if (Input.GetKey(KeyCode.A))
-                {
-                    direction = new Vector2(-1, 0);
-                }
-                
+                //使用CharacterController组件移动
+                float v = Input.GetAxis("Vertical");
+                float h = Input.GetAxis("Horizontal");
+                Vector3 dir = Vector3.right * h + Vector3.forward * v;
                 float speed = currentState == MovementState.Walking ? walkSpeed : runSpeed; //移动速度
-                Vector3 pos = transform.position; //当前位置
-                pos.x += (speed * direction.x * Time.deltaTime);
-                pos.z += (speed * direction.y * Time.deltaTime);
-
-                transform.LookAt(pos);
-                transform.position = pos;
+                dir = dir.normalized; // 归一化移动方向，避免斜向移动速度过快
+                cc.Move(dir * Time.deltaTime * speed);
+                transform.rotation = Quaternion.LookRotation(new Vector3(h,0f,v));
+                
+                // Vector2 direction = Vector2.zero; //移动方向
+                // if (Input.GetKey(KeyCode.W))
+                // {
+                //     direction += new Vector2(0, 1);
+                // }
+                // if (Input.GetKey(KeyCode.S))
+                // {
+                //     direction += new Vector2(0, -1);
+                // }
+                // if (Input.GetKey(KeyCode.D))
+                // {
+                //     direction += new Vector2(1, 0);
+                // }
+                // if (Input.GetKey(KeyCode.A))
+                // {
+                //     direction += new Vector2(-1, 0);
+                // }
+                //
+                // float speed = currentState == MovementState.Walking ? walkSpeed : runSpeed; //移动速度
+                // Vector3 pos = transform.position; //当前位置
+                // pos.x += (speed * direction.x * Time.deltaTime);
+                // pos.z += (speed * direction.y * Time.deltaTime);
+                //
+                // transform.LookAt(pos);
+                // transform.position = pos;
                 
             }
 
